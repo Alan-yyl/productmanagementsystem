e7fc3fd43879e6c60d521c86bd0b7fd02b2dd87c
alan <yyl_email@126,com>
2020-11-23 01:26:33 +0800

    首次提交

---
+++ src/main/java/com/alan/productmanagesystem/ProductmanagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.alan.productmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductmanagesystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alan/productmanagesystem/ProductmanagesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.productmanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmanagesystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
